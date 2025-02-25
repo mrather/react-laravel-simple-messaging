c5073a15d0656f47e0bb9d67cac87813895df6c3
Michelle Rather <madm538@gmail.com>
2025-02-25 15:47:50 -0500

    Added routes with prefixes and tested views for registration and login

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 - php artisan make:controller MessageController
 - php artisan make:migration create_user_messages_table
 - Altered the create_users_table migration to use first name, last name, and removed name. Were this migration already executed. Alternatively, I would create another migration titled add_first_and_last_names_to_users_table and update and do the following:
-- Create two new string columns first_name, last_name in the up() function
+- Created two new string columns first_name, last_name in the up() function
 - Drop the name column in the down() function
-- Update the UserFactory to also use first_name and last_name
+- Updated the UserFactory to also use first_name and last_name
 - Created a user_messages_table
 - Created factories for db seeding samples
 - Created database seeds
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('api/v1')->group(function () {
+
+    // Get all users
+    //Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users');
+    Route::get('/list_all_users', [App\Http\Controllers\UserController::class, 'index'])->name('users');
+
+    // Get a single user
+    //Route::get('/users/{user_id}', [App\Http\Controllers\UserController::class, 'index'])->name('users');
+    Route::get('/list_all_users/{user_id}', [App\Http\Controllers\UserController::class, 'show'])->name('users');
+
+    // Get all messages
+    //Route::get('/messages', [App\Http\Controllers\MessageController::class, 'index'])->name('messages');
+    Route::get('/view_messages/{user_id1}/{user_id2}', [App\Http\Controllers\MessageController::class, 'get_user_messages'])->name('messages');
+
+    // Get a single message
+    //Route::get('/messages/{message_id}', [App\Http\Controllers\MessageController::class, 'show']);
+    Route::get('/view_messages/{message_id}', [App\Http\Controllers\MessageController::class, 'show'])->name('messages');;
+
+    // Send a single message
+    //Route::post('messages/send', [App\Http\Controllers\MessageController::class, 'store'])
+    Route::post('/send_messages', [App\Http\Controllers\MessageController::class, 'store'])->name('messages');;
+
+});
